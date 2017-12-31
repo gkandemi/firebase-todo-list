696adbc82602accab3f4f8c058409ad8cfca476b
gkandemi <gokhan@gkandemir.com>
2017-12-31 03:23:27 +0300

    repo initialize ve Firebase User Register işleminin yapilmasi

---
+++ assets/js/app/login.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    var config = {
+        apiKey: "AIzaSyCPhE4ibN8RXy4MrfXnXUQeG5PUM0zqNfI",
+        authDomain: "fir-todo-list-tutorial.firebaseapp.com",
+        databaseURL: "https://fir-todo-list-tutorial.firebaseio.com",
+        projectId: "fir-todo-list-tutorial",
+        storageBucket: "fir-todo-list-tutorial.appspot.com",
+        messagingSenderId: "535305407723"
+    };
+    firebase.initializeApp(config);
+
+
+})
---
+++ assets/js/app/register.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    var config = {
+        apiKey: "AIzaSyCPhE4ibN8RXy4MrfXnXUQeG5PUM0zqNfI",
+        authDomain: "fir-todo-list-tutorial.firebaseapp.com",
+        databaseURL: "https://fir-todo-list-tutorial.firebaseio.com",
+        projectId: "fir-todo-list-tutorial",
+        storageBucket: "fir-todo-list-tutorial.appspot.com",
+        messagingSenderId: "535305407723"
+    };
+
+    firebase.initializeApp(config);
+
+
+    $("#registerBtn").click(function(){
+
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function(){
+                window.location.href = "index.html";
+            }).catch(function(error){
+                alert(error.message);
+        })
+
+    })
+
+
+})
